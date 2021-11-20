2f6e01793cf49715114fd6826d7079d3c1515b7a
Thomas Dürr <thomduerr@gmail.com>
2021-11-21 00:06:32 +0100

    fastapi application

--- README.md
+++ README.md
@@ -23,3 +23,21 @@ Compare original dataset with cleaned dataset with
 ## 2. Train Prediction Model
 
 Traing of a model using a **random forest classifier**: [train-model.ipynb](./train-model.ipynb)
+
+Test model with:
+
+    import pickle
+    model = pickle.load(open('model/model.pkl', 'rb'))
+    p = model.predict_proba([[2,150,100,23,125,28,1,42]])
+    print(p)
+
+## 3. FastAPI Application
+
+Start API server with
+
+    uvicorn app:app --reload
+
+Open Swagger UI:
+
+    open http://localhost:8000/docs
+
---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+import uvicorn
+import pickle
+from pydantic import BaseModel
+
+app = FastAPI()
+
+model = pickle.load(open("model/model.pkl", "rb"))
+
+
+class Women(BaseModel):
+    pregnancies: int
+    glucose: int
+    bp: int
+    skinthickness: int
+    insulin: int
+    bmi: float
+    dpf: float
+    age: int
+
+
+@app.get("/")
+def redirect():
+    return RedirectResponse(url="/predict")
+
+
+@app.post("/predict")
+def predict(req: Women):
+    preg = req.pregnancies
+    glucose = req.glucose
+    bp = req.bp
+    skinthickness = req.skinthickness
+    insulin = req.insulin
+    bmi = req.bmi
+    dpf = req.dpf
+    age = req.age
+    features = list([preg, glucose, bp, skinthickness, insulin, bmi, dpf, age])
+    predict = model.predict([features])
+    prob = model.predict_proba([features])
+    if predict == 1:
+        return {"prediction": f"positive with {prob[0][1]} probability"}
+    else:
+        return {"prediction": f"negative with {prob[0][0]} probability"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
